710232f42fd72a18ccfb7e7cc983f3a4682e211c
asommacal <sommacalalberto@gmail.com>
2024-01-02 23:07:04 +0100

    add responsive design for header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,29 +1,32 @@
-import React from "react"
+import React, { useState } from 'react';
 import { Link, NavLink } from "react-router-dom"
 
 export default function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+      setMenuOpen(!menuOpen);
+    };
 
     return (
         //position fixed
         <header>
             <Link className="site-logo" to="/">Home</Link>
-            <nav>
-                <NavLink
-                    to="/work"
-                >
-                    Work
-                </NavLink>
-                <NavLink
-                    to="/about"
-                >
-                    About Me
-                </NavLink>
-                <NavLink
-                    to="/contact"
-                >
-                    Contact
-                </NavLink>
-            </nav>
+
+        {/* Hamburger menu button */}
+        <div className="hamburger-menu" onClick={toggleMenu}>
+            <div className={`bar ${menuOpen ? 'open' : ''}`} />
+            <div className={`bar ${menuOpen ? 'open' : ''}`} />
+            <div className={`bar ${menuOpen ? 'open' : ''}`} />
+        </div>
+
+        {/* Navigation */}
+        <nav className={`nav-links ${menuOpen ? 'open' : ''}`}>
+            <NavLink to="/work">Work</NavLink>
+            <NavLink to="/about">About Me</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
+        </nav>
+        
         </header>
         //dummy component
     )
