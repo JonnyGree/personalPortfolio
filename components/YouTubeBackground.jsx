f6e4c1e623b666bac2b04de873f44a03b28cee4b
asommacal <sommacalalberto@gmail.com>
2024-01-02 02:22:28 +0100

    t1

--- components/YouTubeBackground.jsx
+++ components/YouTubeBackground.jsx
@@ -46,26 +46,51 @@ const YouTubeBackground = ({ videoId }) => {
         },
         events: {
           'onReady': onPlayerReady,
-          'onPlaybackRateChange': onPlaybackRateChange,
+          'onStateChange': onPlayerStateChange,
         },
       });
     };
 
     const onPlayerReady = (event) => {
-      // Set the playback speed (0.5 is half speed, 2 is double speed)
-      event.target.setPlaybackRate(0.5);
+      // Autoplay video
+      event.target.playVideo();
     };
 
-    const onPlaybackRateChange = (event) => {
-      if (event.data !== 1) {
-        // If playback rate is not 1, the video has started
+    const onPlayerStateChange = (event) => {
+      // Event triggered when video starts playing
+      if (event.data === window.YT.PlayerState.PLAYING && !videoStarted) {
         setVideoStarted(true);
-
+        
+        // Set the playback speed after 2 seconds
+        setTimeout(() => {
+          setPlaybackSpeed(videoId, 0.5);
+        }, 2000);
+        
         // Hide the loading icon after 4 seconds
         setTimeout(() => {
           setLoadingIconVisible(false);
         }, 4000);
       }
+    };
+
+    const setPlaybackSpeed = (videoId, speed) => {
+      const player = new window.YT.Player('temp-player', {
+        height: 0,
+        width: 0,
+        videoId: videoId,
+        playerVars: {
+          autoplay: 1,
+          controls: 0,
+          enablejsapi: 1,
+          modestbranding: 1,
+          origin: window.location.origin,
+        },
+        events: {
+          'onReady': (event) => {
+            event.target.setPlaybackRate(speed);
+          },
+        },
+      });
     };
 
     loadYouTubeScript();
