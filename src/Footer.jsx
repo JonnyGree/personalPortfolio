e7e9add8fd7bea16d6dc40859c9dc29f640969a4
asommacal <sommacalalberto@gmail.com>
2024-01-06 16:25:34 +0100

    footer first version

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import { Linkedin, Instagram, Github, EnvelopeFill} from 'react-bootstrap-icons';
-import { Link } from "react-router-dom";
 const date = new Date();
 const year = date.getFullYear();
 
@@ -9,46 +8,46 @@ function Footer() {
     return (
       <Container fluid className="footer position-fixed bottom-0">
           <div className="row justify-content-center py-4">
-                <div class="col-auto py-2 px-4">       
+                <div class="col-auto py-2 px-4 ">       
                     <a
-                    href="https://www.linkedin.com/in/soumyajit4419/"
+                    href="https://www.linkedin.com/in/alberto-sommacal-129846151/"
                     target="_blank" 
                     >
-                        <Linkedin width={30} height={30}/>
+                        <Linkedin width={30} height={30} fill="white"/>
                     </a>
                 </div>
                 <div class="col-auto py-2 px-4">                
                     <a 
-                    href="https://github.com/soumyajit4419"
+                    href="https://github.com/JonnyGree"
                     target="_blank" 
                     >
-                        <Github width={30} height={30}/>
+                        <Github width={30} height={30} fill="white"/>
                     </a>
                 </div>
                 <div class="col-auto py-2 px-4"> 
                     <a
-                    href="https://twitter.com/Soumyajit4419"
+                    href="mailto:sommacalalberto@gmail.com"
                     target="_blank" 
                     >
-                        <EnvelopeFill width={30} height={30}/>
+                        <EnvelopeFill width={30} height={30} fill="white"/>
                     </a>
                 </div>
                 <div class="col-auto py-2 px-4"> 
                     <a
-                    href="https://www.instagram.com/soumyajit4419"
+                    href="https://www.instagram.com/alberto.sommacal/?hl=en"
                     target="_blank" 
                     >
-                        <Instagram width={30} height={30}/>
+                        <Instagram width={30} height={30} fill="white"/>
                     </a>
                 </div>
           </div>
-          <div>
-          <div  className="">
-            Designed and Developed by Sommacal Alberto
-          </div>
-          <div className="">
-            Copyright © {year} SMCA
-          </div>
+          <div className="row">
+            <div  className="col-auto">
+                Designed and Developed by Sommacal Alberto
+            </div>
+            <div className="col-auto ms-auto">
+                Copyright © {year} SMCA
+            </div>
         </div>
       </Container>
     );
